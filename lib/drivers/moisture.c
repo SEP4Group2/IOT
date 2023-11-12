42e42d06dda75f0914c8c6ad85b7eae06897e0ff
Walid Mouaaouia <64874079+WalidMoua@users.noreply.github.com>
2023-11-12 15:08:14 +0100

    fix: if else, comment on test

--- lib/drivers/moisture.c
+++ lib/drivers/moisture.c
@@ -23,9 +23,11 @@
  * This function initializes the ADC to read values from the moisture sensor connected to pin PF5 (ADC5).
  */
 
+// TODO -this is a definition for used only to pass the testing, need to be removed/testes with the real sensor
 #define MUX0 1
 #define MUX2 2
 #define ADC5D 5
+// TODO until here -remove 
 
 void moisture_init(void) {
     // Set reference voltage to AVCC and left adjust ADC result
