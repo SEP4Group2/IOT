e2e1581d27bca6fae75edcb69ec3d9f33b90f521
Christopher Hunt <326589@viauc.dk>
2023-11-18 19:05:55 +0100

    Created folder structure

---
+++ controller/water_pump.c
@@ -0,0 +1,11 @@
+//turn on the pump for 1 second via button press
+
+//turn on the pump for 1 second via wifi command
+
+//turn on the pump indefinitely via button press
+
+//turn on the pump indefinitely via wifi command
+
+//turn off the pump via button press
+
+//turn off the pump via wifi command
